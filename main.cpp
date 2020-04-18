8a8300a3871ee2f3323bd002128c625edceeeb34
Kurtis Ingersoll <kurtis.ingersoll@gmail.com>
2020-04-18 13:38:25 -0400

    Included Boost Libraries to CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 
 #include <iostream>
 #include <functional>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
 
 int main()
 {
     [out = std::ref(std::cout << "Hello ")](){ out.get() << "World\n"; }();
+
+    string temp = "TeSt StRiNg";
+    cout << temp << endl;
+    boost::to_lower(temp);
+    cout << temp << endl;
 }
